672c4c8985c69a05930e624390ff5010d04d3264
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-01-09 09:13:28 +0300

    IsAdminUser Added

--- flight/views.py
+++ flight/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import FlightSerializer
 from .models import Flight
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsAdminUser,)  #tuple olduğu belli olsun diye sonuna virgül koyduk 
