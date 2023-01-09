49ab45b8953e7f12fa1502ef4874eb33b3b86235
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-01-09 20:44:03 +0300

    end of the project

--- flight/views.py
+++ flight/views.py
@@ -24,17 +24,13 @@ class FlightView(viewsets.ModelViewSet):
         now = datetime.now()
         current_time = now.strftime('%H:%M:%S')
         today = date.today()
-
         if self.request.user.is_staff:
             return super().get_queryset()
-        
         else:
-            queryset = Flight.objects.filter(date_of_departure__gt=today) #gt = greater than
-            
+            queryset = Flight.objects.filter(date_of_departure__gt=today)
             if Flight.objects.filter(date_of_departure=today):
-                today_qs = Flight.objects.filter(date_of_departure=today).filter(edt__gt=current_time)
-            
-                queryset = queryset.union(today_qs)  # iki queryseti birleştirdim
+                today_qs = Flight.objects.filter(date_of_departure=today).filter(etd__gt=current_time)
+                queryset = queryset.union(today_qs)
             return queryset
 
 
